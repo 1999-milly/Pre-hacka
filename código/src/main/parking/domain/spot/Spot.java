7bef3c97c9d8ffc85c04dc62284be4e379618dff
1999-milly <wemilly.carv@unifesspa.edu.br>
2025-08-04 13:13:53 -0300

    Create Spot.java

---
+++ código/src/main/parking/domain/spot/Spot.java
@@ -0,0 +1,25 @@
+package parking.domain.spot;
+import parking.domain.vehicle.VehicleType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Table(name="spot")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Spot{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+    @Column(name="occupied")
+    private boolean occuppied;
+    @ManyToOne
+    @JoinColumn(name="id_vehicle_type")
+    private VehicleType vehicle;
+    public Spot(VehicleType vehicle){
+        this.vehicle=vehicle;
+    }
+}
