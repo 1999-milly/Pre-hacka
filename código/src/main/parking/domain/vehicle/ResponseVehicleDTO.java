bfdabc998eea10ac112a27e0db0982c579485471
1999-milly <wemilly.carv@unifesspa.edu.br>
2025-08-04 13:24:58 -0300

    Create ResponseVehicleDTO.java

---
+++ código/src/main/parking/domain/vehicle/ResponseVehicleDTO.java
@@ -0,0 +1,11 @@
+package parking.domain.vehicle;
+import lombok.Data;
+@Data
+public class ResponseVehicleDTO{
+    private Integer id;
+    private String vehicle;
+    public ResponseVehicleDTO(VehicleType vehicleType){
+        this.id=vehicleType.getId();
+        this.vehicle=vehicleType.getVehicle();
+    }
+}
