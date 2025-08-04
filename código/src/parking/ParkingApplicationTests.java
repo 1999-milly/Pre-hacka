2f833b61de907cfde256d9e8211a88ea81dfed3c
1999-milly <wemilly.carv@unifesspa.edu.br>
2025-08-04 13:40:58 -0300

    Delete código/src/parking directory

--- código/src/parking/ParkingApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package parking;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest
-class ParkingApplicationTests{
-	@Test
-	void contextLoads(){
-	}
-}
