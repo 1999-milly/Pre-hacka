7118e31b531c9cc0cf60671d90d5172715003461
1999-milly <wemilly.carv@unifesspa.edu.br>
2025-08-04 13:12:56 -0300

    Create ResponseSpotDTO.java

---
+++ código/src/main/parking/domain/spot/ResponseSpotDTO.java
@@ -0,0 +1,14 @@
+package parking.domain.spot;
+import parking.domain.vehicle.VehicleType;
+import lombok.Data;
+@Data
+public class ResponseSpotDTO{
+    private Integer id;
+    private boolean occupied;
+    private VehicleType vehicleType;
+    public ResponseSpotDTO(Spot spot){
+        this.id=spot.getId();
+        this.occupied=spot.isOccuppied();
+        this.vehicleType=spot.getVehicle();
+    }
+}
