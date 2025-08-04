b72c1e73ab831f2f22223e8b3d03c9825354869f
1999-milly <wemilly.carv@unifesspa.edu.br>
2025-08-04 13:34:50 -0300

    Create ParkingApplicationTests.java

---
+++ código/src/parking/ParkingApplicationTests.java
@@ -0,0 +1,9 @@
+package parking;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class ParkingApplicationTests{
+	@Test
+	void contextLoads(){
+	}
+}
