224444ec0a02f9b2d3ed561409196024fd9ae405
1999-milly <wemilly.carv@unifesspa.edu.br>
2025-08-04 13:15:04 -0300

    Update and rename u to RequestFinishTicketDTO.java

---
+++ código/src/main/parking/domain/ticket/RequestFinishTicketDTO.java
@@ -0,0 +1,9 @@
+package parking.domain.ticket;
+import lombok.Data;
+import java.time.LocalDateTime;
+@Data
+public class RequestFinishTicketDTO{
+    private LocalDateTime exit_time;
+    private Float amount;
+    private Integer spot_id;
+}
